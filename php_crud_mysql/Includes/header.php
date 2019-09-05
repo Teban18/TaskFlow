df08f714c6b49fd66e9a0425e9bd62215c36114d
TebanP <aepr_18_@hotmail.com>
2019-09-05 16:55:30 -0500

    first version crud php

---
+++ php_crud_mysql/Includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+    <meta charset = "utf-8">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+    <meta http-equiv = "X-UA-Compatible" content = "ie=edge">
+    <title>TaskFlow</title>
+    <!--  BOOTSTRAP 4  -->
+      <link rel = "stylesheet" href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity = "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin = "anonymous">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" integrity="sha384-rtJEYb85SiYWgfpCr0jn174XgJTn4rptSOQsMroFBPQSGLdOC5IbubP6lJ35qoM9" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <nav class = "navbar navbar-dark bg-dark" >
+      <div class = "container" >
+        <a href = "index.php" class = "navbar-brand" > TaskFlow </a>
+      </div>
+    </nav>
